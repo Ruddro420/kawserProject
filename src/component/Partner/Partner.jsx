1cdb46ee6de8ece0827fb85446686e09bede04fc
ruddro420 <aliruddro@gmail.com>
2023-05-22 17:36:49 +0600

    partner section done

--- src/component/Partner/Partner.jsx
+++ src/component/Partner/Partner.jsx
@@ -1,35 +1,46 @@
 import './Partner.css'
-import useEmblaCarousel from 'embla-carousel-react'
+
 import partner_1 from '../../img/partner/p-1.jpg'
 import partner_2 from '../../img/partner/p-2.jpg'
 import partner_3 from '../../img/partner/p-3.jpg'
 import partner_4 from '../../img/partner/p-4.jpg'
 import partner_5 from '../../img/partner/p-5.jpg'
+import Slider from 'react-slick'
 
 const Partner = () => {
-    const [emblaRef] = useEmblaCarousel({ loop: true, autoplay:true })
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        controls:true
+      };
     return (
-        <div>
-            <div className="embla" ref={emblaRef}>
-                <div className="embla__container">
-                    <div className="embla__slide">
-                        <img src={partner_1} alt="" />
-                    </div>
-                    <div className="embla__slide">
-                        <img src={partner_2} alt="" />
-                    </div>
-                    <div className="embla__slide">
-                        <img src={partner_3} alt="" />
-                    </div>
-                    <div className="embla__slide">
-                        <img src={partner_4} alt="" />
-                    </div>
-                    <div className="embla__slide">
-                        <img src={partner_5} alt="" />
-                    </div>
-                </div>
-            </div>
-        </div>
+        <div className='partner-container'>
+        <Slider {...settings}>
+          <div className='inner-partner-image'>
+            <img src={partner_1} alt="" />
+          </div>
+          <div className='inner-partner-image'>
+            <img src={partner_2} alt="" />
+          </div>
+          
+          <div className='inner-partner-image'>
+            <img src={partner_3} alt="" />
+          </div>
+          <div className='inner-partner-image'>
+            <img src={partner_4} alt="" />
+          </div>
+          
+          <div className='inner-partner-image'>
+            <img src={partner_5} alt="" />
+          </div>
+          
+          
+        </Slider>
+      </div>
     );
 };
 
