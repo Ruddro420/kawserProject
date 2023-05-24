b1aa5870f6de13528993884a6da48b8e9270fa7a
ruddro420 <aliruddro@gmail.com>
2023-05-25 00:51:27 +0600

    responsive fix done

--- src/component/Partner/Partner.jsx
+++ src/component/Partner/Partner.jsx
@@ -13,6 +13,15 @@ const Partner = () => {
     /* controls: true, */
     responsive: [
       {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
         breakpoint: 768,
         settings: {
           slidesToShow: 1,
--- src/component/Working/Working.jsx
+++ src/component/Working/Working.jsx
@@ -21,42 +21,17 @@ const Working = () => {
                             <a data-aos="zoom-in" data-aos-duration={1000 + 100 * i} key={wData.id} href='#' className="">
 
 
-<div className="card w-96 bg-base-100 shadow-xl mt-10">
-  <figure className="pt-10">
-    <img src={wData.src} alt={wData.altText} className="rounded-xl working-img" />
-  </figure>
-  <div className="card-body items-center text-center">
-    <h2 className="card-title">{wData.title}</h2>
-    <p>{wData.description}</p>
-  </div>
-</div>
-
-
-
-
-
-
-
-
-
-                              {/*   <div style={{
-                                    backgroundImage: `url(${wData.src})`,
-                                    backgroundSize: 'cover',
-                                    backgroundRepeat: 'no-repeat',
-                                    backgroundPosition: 'center',
-                                    height: '400px',
-                                    width: '100%',
-                                }} className="gallery-image">
-                                    <div className="working-content">
-                                        <div className="divider"></div>
-                                        <h2>{wData.title}</h2>
-                                        <div className="learn-more">
-                                            <a href={wData.link}>Learn More</a>
-                                        </div>
+                                <div className="card w-96 bg-base-100 shadow-xl mt-10">
+                                    <figure className="pt-10">
+                                        <img src={wData.src} alt={wData.altText} className="rounded-xl working-img" />
+                                    </figure>
+                                    <div className="card-body items-center text-center">
+                                        <h2 className="card-title">{wData.title}</h2>
+                                        <p>{wData.description}</p>
                                     </div>
-                                </div> */}
-                            </a>
+                                </div>
 
+                            </a>
                         )
                     }
                 </div>
